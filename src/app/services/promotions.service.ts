bef4c25c22af35ff20f1eb6506505fc3960a0d93
George Brande <george.brande@gmail.com>
2018-08-07 10:44:18 +0300

    http 1

---
+++ src/app/services/promotions.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import { Promotion } from '../shared/promotion';
+import { PROMOTIONS } from '../shared/promotions';
+
+import { delay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PromotionsService {
+
+  constructor() { }
+
+  getPromotions(): Observable<Promotion[]> {
+    return of(PROMOTIONS).pipe(delay(2000));
+  }
+
+  getPromotion(id: number): Observable<Promotion> {
+    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
+  }
+
+  getFeaturedPromotion(): Observable<Promotion> {
+    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
+}
+}
